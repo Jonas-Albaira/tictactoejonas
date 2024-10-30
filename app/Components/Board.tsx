fc5386da64b862212fe4bf5a83535657b0dff4a8
Jonas-Albaira <jonas.albaira@uoit.net>
2024-10-30 11:52:25 -0400

    tic tac toe

---
+++ app/Components/Board.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import Square from "@/app/Components/Square";
+import {useState} from "react";
+
+export default function Board(){
+
+    const [xIsNext, setXIsNext] = useState(true);
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
+    function handleClick(i: number){
+
+        if (squares[i] || calculateWinner(squares)) {
+            return;
+        }
+
+        const nextSquares = squares.slice(); //creates copy of array
+        nextSquares[i] = "X"; // assigns first element as X?
+
+        if (xIsNext) {
+            nextSquares[i] = "X";
+        } else {
+            nextSquares[i] = "O";
+        }
+
+        setSquares(nextSquares);
+        setXIsNext(!xIsNext);
+
+    }
+
+    const winner = calculateWinner(squares);
+
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+
+
+    return (
+        <div>
+            <div className="status">{status}</div>
+            <div className="board-row">
+                <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+                <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+                <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+            </div>
+            <div className="board-row">
+                <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+                <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+                <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+            </div>
+            <div className="board-row">
+                <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+                <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+                <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+            </div>
+        </div>
+    );
+}
+
+function calculateWinner(squares : any[]){
+
+    //all possible winning combinations
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i=0; i < lines.length; i++){
+
+        const [a,b,c] = lines[i];
+
+        // iterate through all the combinations
+        for (let i=0; i < lines.length; i++){
+            // now check if all variables (all squares are all X or all O's
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+
+    }
+}
---
+++ app/Components/Square.tsx
@@ -0,0 +1,13 @@
+"use client";
+// @ts-ignore
+function Square({value, onSquareClick }) {
+
+    return (
+            <button
+                className="square"
+                onClick={onSquareClick}
+            >{value}
+            </button>
+    );
+}
+export default Square;
